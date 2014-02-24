0b317f346b37630c7594e46407e2adbf027aa758
ARES_HHD <ares.hhd@gmail.com>
2014-02-24 18:23:09 +0800

    update

--- proxy/settings.py
+++ proxy/settings.py
@@ -2,7 +2,7 @@ PID_FILE = '/var/run/collect_profile.pid'
 
 ACCESSLOG_FILE = '/data0/log/scribe/wbcollectmsg_profile/wbcollectmsg_current'
 
-SCRIBE_HOST = 'edgescribe.nb.sinaedge.com'
-SCRIBE_PORT = 1463
+SCRIBE_HOST = 'xxx'
+SCRIBE_PORT = xxx
 SCRIBE_CATEGORY_PLAYBENCH = 'collect_profile'
 
